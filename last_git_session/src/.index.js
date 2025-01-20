62ee213db519c94022a5712ceec621c3648148cf
obisalemhahirwa@gmail.com <obisalemhahirwa@gmail.com>
2025-01-20 10:00:08 +0200

    I added even number

--- last_git_session/src/.index.js
+++ last_git_session/src/.index.js
@@ -1,8 +1,9 @@
 const factotial = (number) => {
-    if (number <= 1) {
+    if (number <= 1)t  {
       return 1;
     }
   
     return number * factotial(number - 1);
   };
   console.log(factotial(10));
+  console.log(even(100, 9, 90, 67, 17, 38, 1, 2, 0,99,11));
